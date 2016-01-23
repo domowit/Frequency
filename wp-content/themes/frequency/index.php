31e109525d0a647577a819c1d2798e071d897e4d
Johnathan Crawford Air <crawford@domowit.com>
2016-01-22 23:23:41 -0600

    update to home

--- wp-content/themes/frequency/index.php
+++ wp-content/themes/frequency/index.php
@@ -8,14 +8,19 @@
 <li>				
 <div class="artistContainer">
 	<div class="information">  
-		<h1>Title</h1>
-		<h3>Date</h3>
-		<h3>location</h3>
+		<h1>Lisa Kaplan and Matthew Duvall</h1>
+		<h3>Tuesday, February 23</h3>
+		<h3>Museum of Contemporary Art. 6 PM
+</h3>
 	</div>
 <div class="slidingContainer"> 	
 	<a href="#/" class="show">More Info</a>
 		<div class="slidingDiv">
-			<div class="text">Fill this space with really interesting content.</div> <a href="#/" class="hide">Hide</a> 
+			<div class="text">
+			Pianist Lisa Kaplan and percussionist Matthew Duvall are founding members of the Grammy-winning contemporary music institution eighth blackbird, one of Chicago’s most successful and staunchest advocates of the new. The ensemble is currently the artist-in-residence at the Museum of Contemporary Art—a rare distinction for a musical entity—so it’s fitting that they kick off the festival in the current digs. This evening they present a typically dynamic program including David Lang’s wed, John Luther Adams’s Red Arc Blue Veil, and Steve Reich’s classic Piano Phase, adopted for toy pianos. The duo has also hinted the program might include some brand-new music composed by Kaplan herself.
+ 
+http://www.eighthblackbird.org/ensemble/
+</div> <a href="#/" class="hide">Hide</a> 
 		</div>
 </div>	
 	<div class="artistBG">
